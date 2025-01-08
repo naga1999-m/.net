0a551c48d46adcb754c9177f4fd057a662fa6c93
naga1999-m <prakash.mandava8@gmail.com>
2025-01-08 00:27:47 -0600

    Add files via upload

---
+++ c beigning/c beigning/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+    {
+        public  static void Main()
+        {
+            Console.WriteLine("please enter FirstName");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("please enter LastName");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("hello {0},{1}", FirstName ,LastName);
+        }
+   
+    }
