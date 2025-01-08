dec7eadaef877362692146e1829ed0d0da540e88
naga1999-m <prakash.mandava8@gmail.com>
2025-01-08 00:26:19 -0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public  static void Main()
+        {
+            Console.WriteLine("please enter first name");
+            string FirstName = Console.Readline();
+
+            Console.WriteLine("please enter last name");
+            string LastName = Console.Readline();
+
+            Console.WriteLine("hello {0},{1}", FirstName ,LastName);
+        }
+    
+    }
+}
